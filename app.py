83c23695a6429f9ddd29e7eab20dda27ca3a358c
Erlampa <lamparcad@gmail.com>
2025-02-27 13:33:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import streamlit as st
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
+from keras.models import load_model
 import numpy as np
 from PIL import Image
 
-model = tf.keras.models.load_model('keras_model.h5')
+np.set_printoptions(suppress=True)
 
+# Load the model
+model = load_model("keras_model.h5", compile=False)
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
 # Funzione per fare la previsione
-def predict_image(img):
-    img = img.resize((224, 224))  # Ridimensiona l'immagine alle dimensioni richieste dal modello
-    img_array = np.array(img)  # Converte l'immagine in un array numpy
-    img_array = np.expand_dims(img_array, axis=0)  # Aggiungi la dimensione del batch
-    img_array = img_array / 255.0  # Normalizza l'immagine se il modello lo richiede
-    
-    # Fai la previsione
-    predictions = model.predict(img_array)
-    return predictions
+
 
 # Imposta l'interfaccia utente
 st.title('Riconoscimento Immagini con Keras e Streamlit')
@@ -31,7 +26,29 @@ if uploaded_image is not None:
     img = Image.open(uploaded_image)
     
     # Fai la previsione
-    predictions = predict_image(img)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+# Replace this with the path to your image
+    image = Image.open(uploaded_image).convert("RGB")
+
+# resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+# turn the image into a numpy array
+    image_array = np.asarray(image)
+
+# Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+# Load the image into the array
+    data[0] = normalized_image_array
+
+# Predicts the model
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
     
     # Mostra la previsione
-    st.write("Previsioni:", predictions)
+    st.write("Previsioni:", class_name)
